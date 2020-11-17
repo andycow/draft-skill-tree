1b7bdd37c66ba879a3c480b4d36707c620809acf
Kyrie-凱里 <randy0815@randyde-imac.ny.corp>
2020-11-17 18:06:49 +0800

    [Modify] add layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+const Home = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: #292929;
+  display: flex;
+  justify-content: center;
+`;
 
-function App() {
+const Left = styled.div`
+  >img{
+    width: 200px;
+  }
+`;
+
+const Right = styled.div`
+  width: 50%;
+  .name{
+    color: #df716f;
+  }
+  .skillContent{
+    background-color: #8d3736;
+  }
+`;
+
+export default ()=>{
+  const skillList = [
+    {
+      name: "",
+      image: "./image/skill1.gif",
+      content: "啟動之後，無論在何地都會進行(25/35/45/55)組硬舉，持續(50/70/80/100)秒。"
+    }
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Home>
+      <Left>
+        <img src="./image/logo.png" />
+      </Left>
+      <Right>
+        <div className="info">
+          <span className="name">張舉人</span>
+        </div>
+        {skillList.map((value, key)=>{
+          return <div className="skillContent">
+            <img src={value.image} />
+            <span>{value.content}</span>
+          </div>
+        })}
+      </Right>
+    </Home>
   );
-}
-
-export default App;
+};
